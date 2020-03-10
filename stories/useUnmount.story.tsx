b00a8f769f36c10717a3c4333626e209de2d0cc6
olivewind <olivewind.wang@gmail.com>
2020-03-10 12:02:27 +0800

    feat(useUnmount): add useUnmount

---
+++ stories/useUnmount.story.tsx
@@ -0,0 +1,28 @@
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Button } from 'antd';
+import * as React from 'react';
+import { useUnmount, useBoolean } from '../src';
+
+function Child() {
+  useUnmount(() => {
+    action('unmont')('unmount');
+  });
+  return (<div style={{ border: '1px solid #000', width: '200px', margin: '20px 0px' }}><h2>I Will Unmount</h2></div>)
+}
+
+const Demo = () => {
+  const [visible, toggle] = useBoolean(true);
+  return (
+    <div style={{ padding: '20px' }}>
+      <Button type="primary" onClick={() => toggle()}>切换</Button>
+      <br />
+      {visible ? <Child /> : null}
+    </div>
+  );
+};
+
+storiesOf('状态', module)
+  .add('useUnmount', Demo, {
+    notes: require('../src/useUnmount/docs.md').default
+  })
